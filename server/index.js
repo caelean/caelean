a6f4f8d073ae519bdfadda0bbc8d4137f26a1959
Caelean Barnes <caeleanb@gmail.com>
2018-01-09 15:14:04 -0800

    fixed static express path

--- server/index.js
+++ server/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const PORT = process.env.PORT || 8080;
 const path = require('path');
 const app = express();
-app.use(express.static(`${__dirname}/../client/dst`));
+app.use(express.static(`${__dirname}/../client/dist`));
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(`${__dirname}/../client/dst/index.html`));
+  res.sendFile(path.resolve(`${__dirname}/../client/dist/index.html`));
 });
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}!`);
