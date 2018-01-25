581a1d80158de9a0b5f5c88c70e0056152d8b2e5
Caelean Barnes <caeleanb@gmail.com>
2018-01-24 17:53:11 -0800

    Move points to constants file

--- client/src/assets/constants.js
+++ client/src/assets/constants.js
@@ -6,6 +6,57 @@ const Constants = {
         CSE: require('./cse_logo.jpg'),
         UCLA: require('./ucla_logo.jpg'),
         UCSD: require('./ucsd_logo.jpg')
+    },
+    POINTS: {
+        CARTA: [
+            'Built Cap Table Access page, increasing requests by over 50%',
+            'Built Investor Relations page, allowing companies to control information rights of their investors',
+            'Developed LP document distribution tool for funds and LLCs',
+            'Created Audit Confirmation Report and user tools to generate company-tailored excel reports'
+        ],
+        ROKU: [
+            'Built fully functional search engine with autosuggest',
+            'Migrated over 40 web pages to international web framework',
+            'Created React components for search box, data router models and autosuggest dropdown',
+            'Fixed bugs and updated assets on front page and others',
+            'Automated syncing version control platforms',
+            'Migrated soundbridge.roku.com, photobridge.roku.com from Rackspace to AWS'
+        ],
+        SECTIONLEAD: [
+            'Personally responsible for a group of 12 students',
+            'Met weekly to help explain key programming concepts',
+            'Held lab hours to help students solve their issues',
+            'Tested and graded code, projects, and exams'
+        ],
+        UCLA: [
+            'Collaborated with Professor Lixia Zhang on the Named Data Networking project building future internet architecture',
+            'Published scientific research poster “Web Based NDN File System Access” showcasing the benefits of NDN compared to IP addresses',
+            'Created database, server, and web page to demonstrate the technology'
+        ],
+        UCSD: [
+            'B.S. Computer Science',
+            'Minor in Cognitive Science',
+            '3.5 GPA',
+            'Earl Warren College Honors Society',
+            'Provost Honors',
+            'Club Water Polo, Club Swim, Sigma Chi'
+        ],
+         SKILLS: [
+            'Python',
+            'Django',
+            'JavaScript',
+            'React',
+            'Java',
+            'HTML',
+            'Node',
+            'Jenkins',
+            'AWS',
+            'CSS',
+            'Shell',
+            'Git',
+            'Asana',
+            'Atlassian'
+        ]
     }
 };
 export default Constants;
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -7,59 +7,10 @@ import Title from './Title.jsx';
 
 class Home extends Component {
     render() {
-        const points = {};
-        points['carta']= [
-            'Built Cap Table Access page, increasing requests by over 50%',
-            'Built Investor Relations page, allowing companies to control information rights of their investors',
-            'Developed LP document distribution tool for funds and LLCs',
-            'Created Audit Confirmation Report and user tools to generate company-tailored excel reports'
-        ];
-        points['roku'] = [
-            'Built fully functional search engine with autosuggest',
-            'Migrated over 40 web pages to international web framework',
-            'Created React components for search box, data router models and autosuggest dropdown',
-            'Fixed bugs and updated assets on front page and others',
-            'Automated syncing version control platforms',
-            'Migrated soundbridge.roku.com, photobridge.roku.com from Rackspace to AWS'
-        ];
-        points['sectionLead'] = [
-            'Personally responsible for a group of 12 students',
-            'Met weekly to help explain key programming concepts',
-            'Held lab hours to help students solve their issues',
-            'Tested and graded code, projects, and exams'
-        ];
-        points['ucla'] = [
-            'Collaborated with Professor Lixia Zhang on the Named Data Networking project building future internet architecture',
-            'Published scientific research poster “Web Based NDN File System Access” showcasing the benefits of NDN compared to IP addresses',
-            'Created database, server, and web page to demonstrate the technology'
-        ];
-        points['skills'] = [  //todo
-            'Python',
-            'Django',
-            'JavaScript',
-            'React',
-            'Java',
-            'HTML',
-            'Node',
-            'Jenkins',
-            'AWS',
-            'CSS',
-            'Shell',
-            'Git',
-            'Asana',
-            'Atlassian'
-        ];
-        points['ucsd'] = [
-            'B.S. Computer Science',
-            'Minor in Cognitive Science',
-            '3.5 GPA',
-            'Earl Warren College Honors Society',
-            'Provost Honors',
-            'Club Water Polo, Club Swim, Sigma Chi'
-        ];
 
-        for(let key in points) {
-            points[key] = points[key].map((point) =>
+        const points_list = Constants.POINTS;
+        for(let key in points_list) {
+            points_list[key] = points_list[key].map((point) =>
                 <li key={point}>
                     {point}
                 </li>
@@ -74,25 +25,25 @@ class Home extends Component {
                     <Title text="Experience" section="home"/>
                 </div>
                 <Segment titleText="Software Engineering Intern, Investor Services"
-                         contentInfo={points['carta']}
+                         contentInfo={points_list.CARTA}
                          image={<img className="photo" src={Constants.IMAGES.CARTA}/>}
                          section="work"
                 />
                 <Segment titleText="Software Engineering Intern, Web Development"
-                         contentInfo={points['roku']}
+                         contentInfo={points_list.ROKU}
                          image={<img className="photo" src={Constants.IMAGES.ROKU}/>}
                 />
                 <Segment titleText="Computer Science Section Lead, UCSD School of Engineering"
-                         contentInfo={points['sectionLead']}
+                         contentInfo={points_list.SECTIONLEAD}
                          image={<img className="photo" src={Constants.IMAGES.CSE}/>}
                 />
                 <Segment titleText="Research Intern, UCLA Engineering Internet Research Lab"
-                         contentInfo={points['ucla']}
+                         contentInfo={points_list.UCLA}
                          image={<img className="photo" src={Constants.IMAGES.UCLA}/>}
 
                 />
                 <Segment titleText="UC San Diego, Jacobs School of Engineering, 2018"
-                         contentInfo={points['ucsd']}
+                         contentInfo={points_list.UCSD}
                          section="education"
                          image={<img className="photo" src={Constants.IMAGES.UCSD}/>}
 
